8d340d9b54740528c4d3652c55dfb4a9c758e226
BorisLos <57643982+BorisLos@users.noreply.github.com>
2019-11-12 22:47:38 +0100

    Add files via upload

---
+++ drzavna_uprava.sql
@@ -0,0 +1,27 @@
+drop database if exists drzavnauprava;
+create database drzavnauprava;
+use drzavnauprava;
+
+create table zupanija(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    zupan varchar(50) not null
+);
+
+create table opcina(
+    sifra int not null primary key auto_increment,
+    zupanija int not null,
+    naziv varchar(50) not null,
+    nacelnik varchar(50) not null
+);
+
+create table mjesto(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    opcina int not null,
+    brojstanovnika int not null
+);
+
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+
+alter table mjesto add foreign key (opcina) references opcina(sifra);
