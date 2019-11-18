29e160a791627e1e0b6828f578aab18fa8ff433d
Boris Lasović <57643982+BorisLos@users.noreply.github.com>
2019-11-18 18:05:02 +0100

    Update drzavna_uprava.sql

--- drzavna_uprava.sql
+++ drzavna_uprava.sql
@@ -25,3 +25,30 @@ create table mjesto(
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+
+#SELECT * FROM zupanija;
+
+INSERT INTO zupanija (sifra,naziv,zupan) values 
+(null,'OBZ','Ivan');
+
+insert into zupanija (sifra,naziv,zupan) values
+(null,'VSZ','Jozo'),
+(null,'BPZ','Marko');
+
+
+#select * from opcina;
+
+insert into opcina (sifra,zupanija,naziv,nacelnik) values
+(null,1,'Opcina Beli Manastir','Robert'),
+(null,1,'Opcina Osijek','Ivica'),
+(null,2,'Opcina Vukovar','Ivan'),
+(null,3,'Opcina Slavonski Brod)','Milica');
+
+#select * from mjesto;
+
+insert into mjesto (sifra,naziv,opcina,brojstanovnika) values
+(null,'Osijek',2,107784),
+(null,'Beli Manastir',1,10068),
+(null,'Vukovar',3,27683),
+(null,'Slavonski Brod',4,59141);
